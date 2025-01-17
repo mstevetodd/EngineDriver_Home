a9eb024845ef9cec8b544aae0fbfbf657d6191d3
mstevetodd <mstevetodd@mstevetodd.com>
2025-01-17 17:50:46 -0500

    first attempt at editing and pushing

--- docs/changes/index.rst
+++ docs/changes/index.rst
@@ -7,8 +7,8 @@ Changes / Updates
 
 .. include:: ../include.rst
 
-Full Change History
--------------------
+Full Change History goes here
+-----------------------------
 
 `view complete changelog <https://raw.githubusercontent.com/JMRI/EngineDriver/master/changelog-and-todo-list.txt>`_
 
--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,7 @@ html_logo = "./_static/images/logo.png"
 
 html_favicon = "./_static/images/favicon.ico"
 
-html_baseurl = "https://flash62au.github.io/EngineDriver_Home/"
+html_baseurl = "https://EngineDriver2.mstevetodd.com/"
 
 html_theme_options = {
     'style_nav_header_background': 'white',
@@ -100,7 +100,7 @@ html_theme_options = {
 
 html_context = {
     'display_github': True,
-    'github_user': 'flash62au',
+    'github_user': 'mstevetodd',
     'github_repo': 'EngineDriver_Home',
     'github_version': 'main/docs/', 
 } 
